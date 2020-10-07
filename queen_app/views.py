5f769947d0bcaa436634b1aaeebdc745b1151950
interpolatio <interpolatio@gmail.com>
2020-10-07 16:17:00 +0300

    Add button "Calculate"

--- queen_app/views.py
+++ queen_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.views import generic
+
 
 def index(request):
-    return HttpResponse("Hello!")
+    if (request.GET.get('work')):
+        print("button")
+    return render(request, 'queen_app/detail.html')
