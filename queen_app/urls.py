937f74fad76a5914c2bacd64528af966ff475fd3
interpolatio <interpolatio@gmail.com>
2020-10-08 18:04:15 +0300

    User authentication process organized.

--- queen_app/urls.py
+++ queen_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    url(r'^login/$', views.user_login, name='login'),
 ]
--- queen_app/views.py
+++ queen_app/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 from django.views import generic
 import queen_app.queen as queen_lib
 from queen_app.models import Desk
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'queen_app/login.html', {'form': form})
 
 
 def index(request):
