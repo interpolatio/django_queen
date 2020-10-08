570381f533875b028c5be28c1a527374c5e514b3
interpolatio <interpolatio@gmail.com>
2020-10-08 16:42:19 +0300

    Made exception handling when writing a duplicate object.

--- queen_app/models.py
+++ queen_app/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.db import IntegrityError
 import json
 
 
 class Desk(models.Model):
-    rank = models.CharField(max_length=300, unique = True)
+    rank = models.CharField(max_length=300, unique=True)
+
+    def save(self, *args, **kwargs):
+        try:
+            super(Desk, self).save(*args, **kwargs)
+        except IntegrityError:
+            pass
 
     def set_rank(self, data):
         self.rank = json.dumps(data)
